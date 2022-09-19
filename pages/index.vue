40fb2dc81eb9e378e3ea41b988dc7cc43f1fb957
Vladislav Knyshov <inova136@gmail.com>
2022-09-19 17:37:43 +0300

    chore: show CoinDeltaPrice at index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial />
+<div style="display: flex; gap: 8px">
+  <CoinDeltaPrice delta-price="400"  />
+  <CoinDeltaPrice delta-price="-45500" large />
+</div>
 </template>
 
 <script>
+import CoinDeltaPrice from '~/components/CoinDeltaPrice.vue';
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { CoinDeltaPrice }
 }
 </script>
