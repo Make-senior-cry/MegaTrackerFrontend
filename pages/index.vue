34cc90e143117da3a42a84dc7f755abaf338bf1a
temerk1n <nikita.verigin23@gmail.com>
2022-09-20 13:50:28 +0300

    feat: add showcase CoinList component

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
   <main>
     <div class="showcase">
-      <CoinDeltaPrice :delta-price="400" />
-      <CoinDeltaPrice :delta-price="-45500" large />
+      <CoinDeltaPrice delta-price="400" />
+      <CoinDeltaPrice delta-price="-45500" large />
     </div>
     <div class="showcase">
-      <ActionButton><TextContent value="Hello fuckers!" /></ActionButton>
+      <ActionButton><TextContent value="Hello fuckers!"/></ActionButton>
     </div>
     <div class="showcase">
-      <PrimaryButton><TextContent value="Primary button" /></PrimaryButton>
+      <PrimaryButton><TextContent value="Primary button"/></PrimaryButton>
     </div>
-    <div class="showcase">
-      <TextInput v-model="inputValue" placeholder="Text input"></TextInput>
+    <div>
+      <CoinListItem :coin="{ ticker: 'BTC', name: 'Bitcoin', currentPrice: 49500, deltaPrice: 480, iconURL: '' }"/>
+    </div>
+    <div>
+      <coin-list :coins="[{ticker: 'BTC', name: 'Bitcoin', currentPrice: 49500, deltaPrice: 480, iconURL: ''},
+      {ticker: 'ETH', name: 'Etherium', currentPrice: 20000, deltaPrice: -200, iconURL: ''}]"/>
     </div>
   </main>
 </template>
@@ -19,29 +23,21 @@
 <script>
 import CoinDeltaPrice from '~/components/CoinDeltaPrice.vue'
 import ActionButton from '~/components/ActionButton.vue'
-import TextContent from '~/components/TextContent.vue'
-import PrimaryButton from '~/components/PrimaryButton.vue'
-import TextInput from '~/components/TextInput.vue'
+import TextContent from '~/components/TextContent.vue';
+import PrimaryButton from '~/components/PrimaryButton.vue';
+import CoinListItem from "~/components/CoinListItem";
+import CoinList from "~/components/CoinList";
 
 export default {
   name: 'IndexPage',
-  components: {
-    CoinDeltaPrice,
-    ActionButton,
-    TextContent,
-    PrimaryButton,
-    TextInput,
-  },
-  data: () => ({
-    inputValue: '',
-  }),
+  components: {CoinList, CoinListItem, CoinDeltaPrice, ActionButton, TextContent, PrimaryButton },
 }
 </script>
 
 <style scoped>
-.showcase {
-  display: flex;
-  gap: 0.5rem;
-  padding: 0.5rem;
-}
+  .showcase {
+    display: flex;
+    gap: 0.5rem;
+    padding: 0.5rem;
+  }
 </style>
