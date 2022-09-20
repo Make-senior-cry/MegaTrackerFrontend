22f5fa706a86358987e7125981457f9af16530c2
Vladislav Knyshov <inova136@gmail.com>
2022-09-20 12:33:25 +0300

    chore: update showcase for PrimaryButton

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,34 @@
 <template>
   <main>
-    <div style="display: flex; gap: 8px">
+    <div class="showcase">
       <CoinDeltaPrice delta-price="400" />
       <CoinDeltaPrice delta-price="-45500" large />
     </div>
-    <div style="display: flex; gap: 8px">
+    <div class="showcase">
       <ActionButton><TextContent value="Hello fuckers!"/></ActionButton>
     </div>
+    <div class="showcase">
+      <PrimaryButton><TextContent value="Primary button"/></PrimaryButton>
+    </div>
   </main>
-</template>
+</template> 
 
 <script>
 import CoinDeltaPrice from '~/components/CoinDeltaPrice.vue'
 import ActionButton from '~/components/ActionButton.vue'
 import TextContent from '~/components/TextContent.vue';
+import PrimaryButton from '~/components/PrimaryButton.vue';
+
 export default {
   name: 'IndexPage',
-  components: { CoinDeltaPrice, ActionButton, TextContent },
+  components: { CoinDeltaPrice, ActionButton, TextContent, PrimaryButton },
 }
 </script>
+
+<style scoped>
+  .showcase {
+    display: flex;
+    gap: 0.5rem;
+    padding: 0.5rem;
+  }
+</style>
