385dd920970f0198c0173556c8aa18884b0dd754
Vladislav Knyshov <inova136@gmail.com>
2022-09-20 12:25:18 +0300

    feat: add TextContent component

---
+++ components/TextContent.vue
@@ -0,0 +1,50 @@
+<script>
+const textType = {
+  H1: 'H1',
+  H2: 'H2',
+  BODY: 'BODY',
+}
+
+const tagMap = {
+  [textType.BODY]: 'span',
+  [textType.H2]: 'h2',
+  [textType.H1]: 'h1',
+}
+
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: textType.BODY,
+    },
+  },
+  render(h) {
+    return h(tagMap[this.variant], { class: [this.variant, 'text'].join(' ') }, this.value)
+  },
+}
+</script>
+
+
+<style scoped>
+  .text {
+    color: var(--black-stroke);
+  }
+
+  .H1 {
+    font-size: 2rem;
+  }
+
+  .H2 {
+    font-weight: 700;
+    font-size: 1.25rem;
+  }
+
+  .BODY {
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+</style>
