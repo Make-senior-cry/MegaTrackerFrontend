6d58f623860a176f1d75f0211f3821f74eb128b0
Vladislav Knyshov <inova136@gmail.com>
2022-09-20 12:26:29 +0300

    chore: add showcase for TextContent and ActionButton

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,21 @@
 <template>
-<div style="display: flex; gap: 8px">
-  <CoinDeltaPrice delta-price="400"  />
-  <CoinDeltaPrice delta-price="-45500" large />
-</div>
+  <main>
+    <div style="display: flex; gap: 8px">
+      <CoinDeltaPrice delta-price="400" />
+      <CoinDeltaPrice delta-price="-45500" large />
+    </div>
+    <div style="display: flex; gap: 8px">
+      <ActionButton><TextContent value="Hello fuckers!"/></ActionButton>
+    </div>
+  </main>
 </template>
 
 <script>
-import CoinDeltaPrice from '~/components/CoinDeltaPrice.vue';
+import CoinDeltaPrice from '~/components/CoinDeltaPrice.vue'
+import ActionButton from '~/components/ActionButton.vue'
+import TextContent from '~/components/TextContent.vue';
 export default {
-    name: "IndexPage",
-    components: { CoinDeltaPrice }
+  name: 'IndexPage',
+  components: { CoinDeltaPrice, ActionButton, TextContent },
 }
 </script>
