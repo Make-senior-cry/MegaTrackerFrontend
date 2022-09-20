96642e27bbfb51dcdb1f266431adcf0151fdad39
Vladislav Knyshov <inova136@gmail.com>
2022-09-20 12:32:55 +0300

    feat: add PrimaryButton component

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button role="button" class="button"><slot></slot></button>
+</template>
+
+<style>
+  .button {
+    background-color: var(--black-stroke);
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    gap: 0.5rem;
+    padding: 0.5rem 1rem;
+    color: var(--white);
+    border: none;
+    border-radius: 0.5rem;
+    width: 100%;
+  }
+</style>
