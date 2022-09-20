2f0a638796aa05c5e67f17dbbd6121377def35a6
temerk1n <nikita.verigin23@gmail.com>
2022-09-20 13:45:05 +0300

    feat: add CoinList.vue component

---
+++ components/CoinList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="listContainer">
+    <ul v-for="coin of coins" :key="coin">
+      <coin-list-item :coin="coin"/>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import CoinListItem from "~/components/CoinListItem";
+export default {
+  name: "CoinList",
+  components: {CoinListItem},
+  props: {
+    coins: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.listContainer {
+  padding: 0.5rem 0;
+  border: 1px solid var(--black-stroke);
+  border-radius: 8px;
+}
+ul {
+  padding: 0;
+  margin: 0;
+}
+</style>
