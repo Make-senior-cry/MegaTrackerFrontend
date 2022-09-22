b6ff6f7d0196926261a031fd10d1d34ab8219d6c
temerk1n <nikita.verigin23@gmail.com>
2022-09-22 18:58:53 +0300

    feat: add CoinHistoryItem.vue

---
+++ components/CoinHistoryItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="coinContainer">
+    <div class="date">
+      <TextContent :value="coinPriceItem.date"/>
+    </div>
+    <div class="priceInfo">
+      <TextContent :value="coinPriceItem.currentPrice" variant="H3" />
+      <CoinDeltaPrice :delta-price="coinPriceItem.deltaPrice" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TextContent from "~/components/TextContent";
+import CoinDeltaPrice from "~/components/CoinDeltaPrice";
+export default {
+  components: {TextContent, CoinDeltaPrice},
+  props: {
+    coinPriceItem: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.coinContainer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  width: 100%;
+  padding: 0.5rem 1rem;
+}
+
+.date {
+  margin: 0.25rem 0;
+  color: var(--gray);
+}
+
+.priceInfo {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 0.5rem;
+  font-size: 1.25rem;
+}
+</style>
