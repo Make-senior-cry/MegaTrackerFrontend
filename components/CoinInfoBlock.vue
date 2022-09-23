af003312d61f70621411ea91a214375e6fe549c2
temerk1n <nikita.verigin23@gmail.com>
2022-09-23 21:21:12 +0300

    feat: add CoinInfoBlock.vue

---
+++ components/CoinInfoBlock.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="coinInfoContainer">
+    <TextContent :value="title"/>
+    <div class="deltaPrice">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextContent from "~/components/TextContent";
+export default {
+  components: {TextContent},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+<!-- TODO: проверить отступы, и класс deltaPrice-->
+<style scoped>
+.coinInfoContainer {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  border: 1px solid var(--black-stroke);
+  border-radius: 0.5rem;
+  padding: 1rem 1rem;
+}
+.deltaPrice {
+  width: fit-content;
+}
+</style>
