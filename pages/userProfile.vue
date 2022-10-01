81c0f9e776a28f8d35f28ed528d2c472c817b0ac
Vladislav Knyshov <inova136@gmail.com>
2022-10-02 00:07:39 +0300

    feat: add Profile page

---
+++ pages/userProfile.vue
@@ -0,0 +1,26 @@
+<template>
+  <DefaultLayout>
+    <TextContent variant="H1" :value="userEmail" />
+    <ActionButton>
+      <LockOutline />
+      Я хочу поменять пароль
+    </ActionButton>
+  </DefaultLayout>
+</template>
+
+<script>
+import LockOutline from 'vue-material-design-icons/LockOutline.vue'
+import TextContent from '../components/TextContent.vue'
+import DefaultLayout from '~/components/DefaultLayout.vue'
+import AuthService from '~/services/AuthService'
+import ActionButton from '~/components/ActionButton.vue'
+
+export default {
+  components: { TextContent, DefaultLayout, ActionButton, LockOutline },
+  computed: {
+    userEmail() {
+      return AuthService.getUserEmail()
+    },
+  },
+}
+</script>
