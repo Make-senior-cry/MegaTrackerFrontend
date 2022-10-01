1bc319b6dc5dfa95e4fc2ec4a449a502c9b80182
Vladislav Knyshov <inova136@gmail.com>
2022-10-02 00:07:01 +0300

    feat: add loading state to CoinList

--- components/CoinList.vue
+++ components/CoinList.vue
@@ -1,8 +1,12 @@
 <template>
   <!-- TODO: определить, массив каких сущностей принннимает этот компонент -->
   <ul class="coinListContainer">
+    <div v-if="loading" class="loadingContainer">
+      <Loading class="loading" />
+    </div>
     <coin-list-item
       v-for="coin of coins"
+      v-else
       :key="coin.ticker"
       :coin="coin"
       :show-favourite="showFavourite"
@@ -12,17 +16,22 @@
 </template>
 
 <script>
+import Loading from 'vue-material-design-icons/Loading.vue'
 import CoinListItem from '~/components/CoinListItem'
 
 export default {
   name: 'CoinList',
-  components: { CoinListItem },
+  components: { CoinListItem, Loading },
   props: {
     coins: {
       type: Array,
       required: true,
     },
     showFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -36,5 +45,27 @@ export default {
   border: 1px solid var(--black-stroke);
   border-radius: 0.5rem;
   margin: 0;
+}
+
+.loadingContainer {
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading {
+  animation-name: spin;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 }
 </style>
