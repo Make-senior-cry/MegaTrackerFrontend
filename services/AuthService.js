8e037d2bbd34e4ad2ac1adf1676e0b427d6eaca5
Vladislav Knyshov <inova136@gmail.com>
2022-10-01 21:56:49 +0300

    feat: add Auth service

---
+++ services/AuthService.js
@@ -0,0 +1,31 @@
+import { setAuthHeader } from '~/api/http'
+import parseJWT from '~/utils/parseJWT'
+
+export default class AuthServie {
+  static localStorageAuthItemName = 'auth'
+
+  static getTokens() {
+    const payload = localStorage.getItem(this.localStorageAuthItemName)
+    if (!payload) return null
+
+    return JSON.parse(payload)
+  }
+
+  static setUser(accessToken, refreshToken) {
+    const payload = JSON.stringify({ accessToken, refreshToken })
+    setAuthHeader(accessToken)
+    localStorage.setItem(this.localStorageAuthItemName, payload)
+  }
+
+  static removeUser() {
+    localStorage.removeItem(this.localStorageAuthItemName)
+    setAuthHeader(null)
+  }
+
+  static getUserEmail() {
+    const data = AuthServie.getTokens()
+    if (!data) return null
+
+    return parseJWT(data.accessToken).email
+  }
+}
