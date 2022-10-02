959a8e3e9bbac84b8276f0a31883e644de6724a8
Vladislav Knyshov <inova136@gmail.com>
2022-10-02 11:35:52 +0300

    feat: add logout button

--- pages/userProfile.vue
+++ pages/userProfile.vue
@@ -1,5 +1,8 @@
 <template>
   <DefaultLayout>
+    <template #actions>
+      <button @click="logout"><AccountMinusOutline /></button>
+    </template>
     <TextContent variant="H1" :value="userEmail" />
     <ActionButton>
       <LockOutline />
@@ -10,16 +13,29 @@
 
 <script>
 import LockOutline from 'vue-material-design-icons/LockOutline.vue'
+import AccountMinusOutline from 'vue-material-design-icons/AccountMinusOutline.vue'
 import TextContent from '../components/TextContent.vue'
 import DefaultLayout from '~/components/DefaultLayout.vue'
 import AuthService from '~/services/AuthService'
 import ActionButton from '~/components/ActionButton.vue'
 
 export default {
-  components: { TextContent, DefaultLayout, ActionButton, LockOutline },
+  components: {
+    TextContent,
+    DefaultLayout,
+    ActionButton,
+    LockOutline,
+    AccountMinusOutline,
+  },
   computed: {
     userEmail() {
       return AuthService.getUserEmail()
+    },
+  },
+  methods: {
+    logout() {
+      AuthService.removeUser()
+      this.$router.replace('/')
     },
   },
 }
