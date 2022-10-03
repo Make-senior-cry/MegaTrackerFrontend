14086010add7c05e86b86919a01915e480f676a5
Vladislav Knyshov <inova136@gmail.com>
2022-10-03 21:39:35 +0300

    feat: add coin page

---
+++ pages/_ticker/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <DefaultLayout>
+    <CoinPageInfoSection :ticker="ticker" />
+    <TextContent variant="H2" value="История цен" />
+    <CoinPageHistorySection :ticker="ticker" />
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '~/components/DefaultLayout.vue'
+import TextContent from '~/components/TextContent.vue'
+import CoinPageInfoSection from '~/components/CoinPageInfoSection.vue'
+import CoinPageHistorySection from '~/components/CoinPageHistorySection.vue'
+
+export default {
+  components: {
+    DefaultLayout,
+    TextContent,
+    CoinPageInfoSection,
+    CoinPageHistorySection,
+  },
+  computed: {
+    ticker() {
+      return this.$route.params.ticker
+    },
+  },
+}
+</script>
