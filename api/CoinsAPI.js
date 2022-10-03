e1c64148b32cec0af09e2388b924fe4975919076
Vladislav Knyshov <inova136@gmail.com>
2022-10-03 21:36:57 +0300

    feat: add getHistoryByTicker method

--- api/CoinsAPI.js
+++ api/CoinsAPI.js
@@ -1,5 +1,6 @@
 import http from './http'
 import { CoinListItemDTO } from '~/dto/CoinListItemDTO'
+import CointHistoryListItemDTO from '~/dto/CoinHistoryListItemDto'
 
 export default class CoinsAPI {
   static async getCoins({ page = 1, pageSize = 6 }) {
@@ -12,5 +13,15 @@ export default class CoinsAPI {
       pageCount,
       coins: coins.map((coin) => new CoinListItemDTO(coin)),
     }
+  }
+
+  static async getCoinByTiker(ticker) {
+    const response = await http.get(`/coins/${ticker}`)
+    return response.data
+  }
+
+  static async getHistoryByTicker(ticker) {
+    const response = await http.get(`/coins/${ticker}/history`)
+    return response.data.map((item) => new CointHistoryListItemDTO(item))
   }
 }
