f1df6ad900af82ae4eed6cf441bb0336a1d44575
Vladislav Knyshov <inova136@gmail.com>
2022-10-03 21:35:10 +0300

    feat: add LoadingSpinner component

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loadingContainer">
+    <Loading class="loading" />
+  </div>
+</template>
+
+<script>
+import Loading from 'vue-material-design-icons/Loading.vue'
+export default {
+  components: { Loading },
+}
+</script>
+
+<style scoped>
+.loadingContainer {
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading {
+  animation-name: spin;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+</style>
