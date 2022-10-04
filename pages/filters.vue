78edbc9738c09663de0a63e07cb9dd44a6dcd638
Vladislav Knyshov <inova136@gmail.com>
2022-10-04 13:07:42 +0300

    feat: add data field to filters

--- pages/filters.vue
+++ pages/filters.vue
@@ -1,10 +1,17 @@
 <template>
-  <DefaultLayout> Фильтры </DefaultLayout>
+  <DefaultLayout>
+
+  </DefaultLayout>
 </template>
 
 <script>
 import DefaultLayout from '~/components/DefaultLayout.vue'
 export default {
   components: { DefaultLayout },
+  data: () => ({
+    minPrice: 0,
+    maxPrice: 0,
+    isRising: false,
+  })
 }
 </script>
